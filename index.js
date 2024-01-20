7b61f6e66e13790dfe17a87d059185e5499b86c6
Shipan Mallik <mshipan657@gmail.com>
2024-01-20 23:50:17 +0600

    create aboutMeCollection, get about-me api and put about-me api

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  // methods: ["GET", "POST", "PUT", "DELETE"],
+  optionSuccessStatus: 200,
+};
+
 //middlewares
-app.use(cors());
+app.use(cors(corsConfig));
+app.options("", cors(corsConfig));
 app.use(express.json());
 
 //mongodb start
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ow0mqis.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1oh7p7d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,10 +35,48 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    //collections start
-    //collections end
+    // collection start
+    const aboutMeCollection = client.db("portfolio-v2").collection("about-me");
+    // collection end
 
     //APIs start
+
+    // Get/view about-me Api
+    app.get("/about-me", async (req, res) => {
+      const result = await aboutMeCollection.find().toArray();
+      res.send(result);
+    });
+
+    // PUT/update about-me Api
+    app.put("/about-me/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateAboutMe = req.body;
+      const newAboutMe = {
+        $set: {
+          name: updateAboutMe.name,
+          role: updateAboutMe.role,
+          about: updateAboutMe.about,
+          mobile: updateAboutMe.mobile,
+          email: updateAboutMe.email,
+          address: updateAboutMe.address,
+          website: updateAboutMe.website,
+          maplink: updateAboutMe.maplink,
+          watchTheVideo: updateAboutMe.watchTheVideo,
+          facebook: updateAboutMe.facebook,
+          twitter: updateAboutMe.twitter,
+          github: updateAboutMe.github,
+          linkedin: updateAboutMe.linkedin,
+        },
+      };
+      const result = await aboutMeCollection.updateOne(
+        filter,
+        newAboutMe,
+        options
+      );
+      res.send(result);
+    });
     //APIs end
 
     // Send a ping to confirm a successful connection
